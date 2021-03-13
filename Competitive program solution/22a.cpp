9091fa3c3f8678850138434d587ea83742b02382
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2021-03-13 10:30:15 +0600

    qq

---
+++ Competitive program solution/22a.cpp
@@ -0,0 +1,59 @@
+//        ** Sumonta Saha Mridul **                                    SWE - SUST 
+/*     
+
+         ######    ##     ##  ##     ##   #######   ##    ##   ########      ###
+        ##    ##   ##     ##  ###   ###  ##     ##  ###   ##      ##        ## ##
+        ##         ##     ##  #### ####  ##     ##  ####  ##      ##       ##   ##
+         ######    ##     ##  ## ### ##  ##     ##  ## ## ##      ##      ##     ##
+              ##   ##     ##  ##     ##  ##     ##  ##  ####      ##      #########
+        ##    ##   ##     ##  ##     ##  ##     ##  ##   ###      ##      ##     ##
+         ######     #######   ##     ##   #######   ##    ##      ##      ##     ##
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define F(i,s,e) for(ll i=s;i<e;++i)
+#define ll long long
+#define pb push_back
+using vll = vector<ll>;
+#define P(str) cout << str << endl
+#define endl '\n'
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)  
+   
+const int N = int(1e5 + 3);
+
+
+int main()
+{ 
+    fast;
+
+set <int> s;
+set <int>::iterator it ;
+
+int n ;
+cin >> n;
+
+for (int i = 1 ; i<= n ; i++)
+{
+    int x ;
+    cin >> x ;
+    s.insert(x);
+}
+
+it = s.begin();
+if(n==1 || s.size()<2) cout<<"NO";
+else
+{
+    it++;
+cout<<*it;
+}
+
+
+
+ 
+}
