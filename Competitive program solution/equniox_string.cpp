64feabb4aff4fa0813506c9e26ab061b2172b683
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2021-05-04 17:24:44 +0600

    may 2nd

---
+++ Competitive program solution/equniox_string.cpp
@@ -0,0 +1,91 @@
+//        ** Sumonta Saha Mridul **                                    SWE - SUST
+/*     
+
+ *        ######    ##     ##  ##     ##   #######   ##    ##   ########      ###
+ !       ##    ##   ##     ##  ###   ###  ##     ##  ###   ##      ##        ## ##
+ ?       ##         ##     ##  #### ####  ##     ##  ####  ##      ##       ##   ##
+ *        ######    ##     ##  ## ### ##  ##     ##  ## ## ##      ##      ##     ##
+ !             ##   ##     ##  ##     ##  ##     ##  ##  ####      ##      #########
+ ?       ##    ##   ##     ##  ##     ##  ##     ##  ##   ###      ##      ##     ##
+ *        ######     #######   ##     ##   #######   ##    ##      ##      ##     ##
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define pll pair<long, long>
+#define mp(a, b) make_pair(a, b)
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<pii>
+#define Mi map<int, int>
+#define mii map<pii, int>
+#define all(a) (a).begin(), (a).end()
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define endl '\n'
+#define Y cout << "YES\n"
+#define No cout << "NO\n"
+
+#define F(i, s, e) for (ll i = s; i < e; ++i)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rem(i, a, b) for (int i = a; i > b; i--)
+#define P(str) cout << str << endl
+
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define mod 1000000007
+#define INF numeric_limits<ll>::max();
+#define NINF numeric_limits<ll>::min();
+const int N = int(1e5 + 3);
+
+int main()
+{
+    fast;
+
+    ll t;
+    cin >> t;
+
+    string arr = "EQUINOX";
+
+    while (t--)
+    {
+        ll n, a, b;
+        ll ans1 = 0, ans2 = 0;
+
+        cin >> n >> a >> b;
+
+        for (ll i = 0; i < n; i++)
+        {
+            string s;
+            cin >> s;
+            ll f = 0;
+
+            for (ll j = 0; j < arr.size(); j++)
+            {
+                if (arr[j] == s[0])
+                    f = 1;
+            }
+            if (f)
+                ans1 += a;
+            else
+                ans2 += b;
+        }
+        if (ans1 == ans2)
+            cout << "Draw\n";
+        else if (ans1 > ans2)
+            cout << "Sarthak\n";
+        else
+            cout << "Anuradha\n";
+    }
+}
---
+++ Competitive program solution/picking_numbers.cpp
@@ -0,0 +1,71 @@
+//        ** Sumonta Saha Mridul **                                    SWE - SUST
+/*     
+
+ *        ######    ##     ##  ##     ##   #######   ##    ##   ########      ###
+ !       ##    ##   ##     ##  ###   ###  ##     ##  ###   ##      ##        ## ##
+ ?       ##         ##     ##  #### ####  ##     ##  ####  ##      ##       ##   ##
+ *        ######    ##     ##  ## ### ##  ##     ##  ## ## ##      ##      ##     ##
+ !             ##   ##     ##  ##     ##  ##     ##  ##  ####      ##      #########
+ ?       ##    ##   ##     ##  ##     ##  ##     ##  ##   ###      ##      ##     ##
+ *        ######     #######   ##     ##   #######   ##    ##      ##      ##     ##
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define pll pair<long, long>
+#define mp(a, b) make_pair(a, b)
+#define vi arrtor<int>
+#define vll arrtor<ll>
+#define vii arrtor<pii>
+#define Mi map<int, int>
+#define mii map<pii, int>
+#define all(a) (a).begin(), (a).end()
+#define f first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define endl '\n'
+#define Y cout << "YES\n"
+#define No cout << "NO\n"
+
+#define F(i, s, e) for (ll i = s; i < e; ++i)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rem(i, a, b) for (int i = a; i > b; i--)
+#define P(str) cout << str << endl
+
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define mod 1000000007
+#define INF numeric_limits<ll>::max();
+#define NINF numeric_limits<ll>::min();
+const int N = int(1e5 + 3);
+
+int main()
+{
+    fast;
+    int n, count, maxi = 0;
+
+    vector<int> arr(100);
+
+    cin >> n;
+    while (n--)
+    {
+        cin >> count;
+        arr[count]++;
+    }
+    for (int i = 1; i < 100; i++)
+    {
+        maxi = max(maxi, arr[i] + arr[i - 1]);
+    }
+    cout << maxi << endl;
+}
+
