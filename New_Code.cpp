c49f4fe0ce88b1063ca31a77f21340dbfb3f5a58
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2022-09-01 22:39:58 +0600

    new Templete added

---
+++ New_Code.cpp
@@ -0,0 +1,34 @@
+//        ** Sumonta Saha Mridul **                                    SWE - SUST
+/*
+
+ *        ######    ##     ##  ##     ##   #######   ##    ##   ########      ###
+ !       ##    ##   ##     ##  ###   ###  ##     ##  ###   ##      ##        ## ##
+ ?       ##         ##     ##  #### ####  ##     ##  ####  ##      ##       ##   ##
+ *        ######    ##     ##  ## ### ##  ##     ##  ## ## ##      ##      ##     ##
+ !             ##   ##     ##  ##     ##  ##     ##  ##  ####      ##      #########
+ ?       ##    ##   ##     ##  ##     ##  ##     ##  ##   ###      ##      ##     ##
+ *        ######     #######   ##     ##   #######   ##    ##      ##      ##     ##
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define sp " "
+#define enter cout << endl;
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+    
+
+int main()
+{
+    fast;
+    
+}
+
+//* char a = 'A';   int num = (int) a;
+//* char a = '2';   int num = a-48;
+//* char a = '8';   int num = a - '0' ;  (  s[i]-'0' == 8 )
